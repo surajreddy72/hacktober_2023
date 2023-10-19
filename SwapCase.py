6e00f6f0dc7700952f670d52f72f257680fd394c
Suraj Reddy <82399778+surajreddy72@users.noreply.github.com>
2023-10-19 18:00:07 +0530

    Create SwapCase.py
    Intial Commit

---
+++ SwapCase.py
@@ -0,0 +1,8 @@
+sentence = "i love CONTRIBUTE"
+
+# converts first character to uppercase and others to lowercase
+capitalized_string = sentence.capitalize()
+
+print(capitalized_string)
+
+# Output: I love contribute
